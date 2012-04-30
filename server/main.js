b0da035aef3188b0616cc60306e2706224c419a9
Jose A. Muniz Navarro <jmuniz@li433-250.members.linode.com>
2012-04-30 00:19:17 -0400

    Implement server sice venue_find

--- server/main.js
+++ server/main.js
@@ -1,62 +1,55 @@
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var fs = require('fs');
-
-var nominatim_url = "http://nominatim.openstreetmap.org/search?format=json&q="; 
-var positions = 
- '[ { "latitude": 42.3605, "longitude": -71.0593 }, { "latitude": 42.360, "longitude": -71.0700 } ]'; 
-
-
-function getPositions(address) {
-}
+var jQuery = require('jquery');
+ 
 http.createServer(
 function (req, res) {
     var url_parts = url.parse(req.url, true);
     var address = url_parts.query.address;
-    console.log("Processing request for " + url_parts.query.address);
+    console.log("Processing venue_find request for " + url_parts.query.address);
 
     res.writeHead(200, 
         {'Content-Type' : 'application/json',
          'Access-Control-Allow-Origin': '*'});
 
-        //path: "/search?format=json&q='500 Memorial Drive, Cambridge, MA. USA. 02139'",
     var options = {
-        host: "nominatim.openstreetmap.org",
-        port: 80,
-        path: "/search?format=json&q='" + escape(address) + "'", 
+        host: "api.foursquare.com",
+        path: "/v2/venues/search?near='" + escape(address) + "'" + 
+	      "&client_id=GXSFY2MK32QDGOZW4OT3VKFFYBJRZAQWKJVJGCYTS3MZAQ4L&client_secret=N3JVG0VY3XW0020PPIZXTRVSWCH3TRAZOIUBR35LMQVHAOCG&v=20120429", 
         method: "GET"
     };
-    
-    var nominatim_response = "";
+   
+    var foursquare_response = "";
 
-    http.get(
+    https.get(
         options,
-        function(nominatim_res) {
-            nominatim_res.on('data', function (chunk) {
-                nominatim_response += chunk;
+        function(foursquare_res) {
+            foursquare_res.on('data', function (chunk) {
+                foursquare_response += chunk;
             });
-            nominatim_res.on('end', function (data) {
-                parsed_result = JSON.parse(nominatim_response);
-                /* TODO: May receive more (or less) than one result */
-                var points = []; 
+            foursquare_res.on('end', function (data) {
+                parsed_result = JSON.parse(foursquare_response);
+		var response = {}; 
 
-		try { 
-		    for (var i = 0; i < parsed_result.length; i++) { 
-		        var point = {
-			    "latitude":parsed_result[i].lat,
-			    "longitude":parsed_result[i].lon
-			    };
-                
-			    points.push(point);
-			    console.log('Returning (' + point.latitude + ', ' + point.longitude + ')'); 
-		    }
+		var venues = jQuery.map(parsed_result.response.venues, function(val, i) {
+			var address_entry = {};
+			address_entry.latitude = val.location.lat;
+			address_entry.longitude= val.location.lng;
+			address_entry.name = val.name;
 
-		    res.end(JSON.stringify(points));
-		} 
-		catch (err) {
-		    console.log('Returning []')
-	 	    res.end('[]');
+			return address_entry;
+		});
+	
+		var geopoint = {
+			"latitude":parsed_result.response.geocode.feature.geometry.center.lat,
+			"longitude":parsed_result.response.geocode.feature.geometry.center.lng
 		}
+		response.geopoint = geopoint;
+		response.venues = venues;
+		
+		res.end(JSON.stringify(response));
 
             });
         } 
@@ -70,13 +63,12 @@ http.createServer(
 function (req, res) {
     var url_parts = url.parse(req.url, true);
     var address = url_parts.query.address;
-    console.log("Processing request for " + url_parts.query.address);
+    console.log("Processing map_find request for " + url_parts.query.address);
 
     res.writeHead(200, 
         {'Content-Type' : 'application/json',
          'Access-Control-Allow-Origin': '*'});
 
-        //path: "/search?format=json&q='500 Memorial Drive, Cambridge, MA. USA. 02139'",
     var options = {
         host: "nominatim.openstreetmap.org",
         port: 80,
@@ -122,3 +114,5 @@ function (req, res) {
 
 }).listen(3000); 
 
+
+console.log("listening");
