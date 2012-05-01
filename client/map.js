19cc17ae5e57786cc7c6fd08a49a24a7b24a1930
Julián Hernández Muñoz <jchernan@alum.mit.edu>
2012-04-30 20:40:18 -0700

    Same as last commit message.

--- client/map.js
+++ client/map.js
@@ -24,7 +24,19 @@ MapApp.mapZooms = {
 
 MapApp.tileStreamServer = "http://www.aeternitatis.org:8888";
 MapApp.addressServer = "http://www.aeternitatis.org/map_find";
+MapApp.venueServer = "http://www.aeternitatis.org/venue_find";
 MapApp.tileStreamUrl = MapApp.tileStreamServer + "/v2/boston/{z}/{x}/{y}.png";
+
+MapApp.inBounds = function(point) {
+    if (point.latitude >= MapApp.mapPoints.lowerRight.lat 
+          && point.latitude <= MapApp.mapPoints.upperLeft.lat 
+          && point.longitude >= MapApp.mapPoints.upperLeft.lon 
+          && point.longitude <= MapApp.mapPoints.lowerRight.lon) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // tile layer
 var tileLayer = new L.TileLayer(
@@ -54,26 +66,45 @@ function find_and_display_address() {
         "address": input_field
     };
 
-    /* Show progress bar */
+    // Show progress bar 
     $('#address_search_field').css('background-image', 'url("ajax-loader.gif")');
     layerGroup.clearLayers();
 
+    // query the address server
     $.getJSON(MapApp.addressServer, address, function(data) {
-        $('#address_search_field').css('background-image', '');
-
+        
         for (var i = 0; i < data.length; i++) {
             var point = data[i];
-            if (point.latitude >= MapApp.mapPoints.lowerRight.lat 
-                && point.latitude <= MapApp.mapPoints.upperLeft.lat 
-                && point.longitude >= MapApp.mapPoints.upperLeft.lon 
-                && point.longitude <= MapApp.mapPoints.lowerRight.lon) {
-
-                var markerLoc = new L.LatLng(point.latitude, point.longitude);
-                var marker = new L.Marker(markerLoc);
-                layerGroup.addLayer(marker);
-                map.setView(markerLoc, MapApp.mapZooms.defaultFound);
+            if (MapApp.inBounds(point)) {
+                var markerLoc = addMarker(point, null, "pink");
+                map.setView(markerLoc, MapApp.mapZooms.foundZoom);
             }
         }
+    }).error(function(data) {
+        // if there is an error, set view at the default center point
+        map.setView(defaultCenter, MapApp.mapZooms.defaultZoom).addLayer(tileLayer);
+        console.log("Error: " + data.statusText);
+        console.log("Response text: " + data.responseText);
+        $('#address_search_field').css('background-image', '');
+    });
+
+    // query the venues server
+    $.getJSON(MapApp.venueServer, address, function(data) {
+
+        if (MapApp.inBounds(data.geopoint)) {
+            var markerLoc = addMarker(data.geopoint, null, "purple");
+            map.setView(markerLoc, MapApp.mapZooms.foundZoom);
+        }
+
+        for (var i = 0; i < data.venues.length; i++) {
+            var point = data.venues[i];
+            if (MapApp.inBounds(point)) {
+                addMarker(point, point.name, "blue");
+            }
+        }
+
+        $('#address_search_field').css('background-image', '');
+
     }).error(function(data) {
         // if there is an error, set view at the default center point
         map.setView(defaultCenter, MapApp.mapZooms.defaultZoom).addLayer(tileLayer);
@@ -85,3 +116,22 @@ function find_and_display_address() {
     return false;
 }
 
+function addMarker(point, name, color) {
+    var markerLoc = new L.LatLng(point.latitude, point.longitude);
+    var url = 'markers/color-pin.png';
+    var icon = new MapApp.MarkerIcon(url.replace("color", color));
+    var marker = new L.Marker(markerLoc, {icon: icon});
+    if (name != null) {
+        marker.bindPopup(name).openPopup();
+    }
+    layerGroup.addLayer(marker);
+    return markerLoc;
+}
+
+MapApp.MarkerIcon = L.Icon.extend({
+    iconUrl: 'markers/black-pin.png',
+    shadowUrl: null,
+    iconSize: new L.Point(16, 28),
+    iconAnchor: new L.Point(8, 28),
+    popupAnchor: new L.Point(0, -28)
+});
